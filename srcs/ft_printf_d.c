6b9e85aaa44605e4d47f9d5af7c7392c02e93070
Vincent LE GAL <vle-gal@e1r11p2.42.fr>
2018-01-19 16:14:42 +0100

    is space ok?

--- srcs/ft_printf_d.c
+++ srcs/ft_printf_d.c
@@ -6,7 +6,7 @@
 /*   By: anonymou <anonymou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/28 09:15:06 by anonymou          #+#    #+#             */
-/*   Updated: 2018/01/19 16:03:51 by vle-gal          ###   ########.fr       */
+/*   Updated: 2018/01/19 16:13:38 by vle-gal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,8 +72,7 @@ static char			*ft_dstrz(ssize_t d, t_struct *para)
 void				ft_d(t_struct *para, char *d)
 {
 	int		len;
-
-	para->p_ == 0 && para->s_ == 1 ?ft_putchar(' '): 0;
+	para->p_ == 0 && para->s_ == 1 && d[0] != '-' ?ft_putchar(' '): 0;
 	para->p_ == 0 && para->s_ == 1 ?para->s_--: 0;
 	para->p_ == 0 && para->s_ == 1 ?para->res++: 0;
 	para->d_ != -1 && d[0] == '0' ? d[0] = '\0' : 0;
